4f35d907aa9e8ea96c247a05400f82e2f9c74111
Mcat12 <newtoncat12@yahoo.com>
2017-06-30 14:14:04 -0400

    Make header pretty
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -26,7 +26,11 @@ class Header extends Component {
     return (
       <header className="app-header navbar">
         <button className="navbar-toggler mobile-sidebar-toggler d-lg-none" onClick={this.mobileSidebarToggle} type="button">&#9776;</button>
-        <a className="navbar-brand" href="https://pi-hole.net">Pi-hole</a>
+        <a className="navbar-brand" href="https://pi-hole.net">
+          <span style={{color: "white", paddingLeft: "45px", lineHeight: "40px"}}>
+            Pi-<b>hole</b>
+          </span>
+        </a>
         <ul className="nav navbar-nav d-md-down-none mr-auto">
           <li className="nav-item">
             <button className="nav-link navbar-toggler sidebar-toggler" type="button" onClick={this.sidebarToggle}>&#9776;</button>
