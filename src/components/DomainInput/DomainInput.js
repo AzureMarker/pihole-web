e14e7e6c08e9a7cc0ef43511c8080fba7a87a43f
Mcat12 <newtoncat12@yahoo.com>
2017-08-11 22:04:13 -0400

    Clear domain input on submit
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/components/DomainInput/DomainInput.js
+++ src/components/DomainInput/DomainInput.js
@@ -2,21 +2,33 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class DomainInput extends Component {
+  state = {
+    domain: ""
+  };
+
   constructor(props) {
     super(props);
+
+    this.handleChange = this.handleChange.bind(this);
     this.onAdd = this.onAdd.bind(this);
   }
 
+  handleChange(e) {
+    this.setState({ domain: e.target.value });
+  }
+
   onAdd() {
-    this.props.onAdd(this.refs.input.value);
+    this.props.onAdd(this.state.domain);
+    this.setState({ domain: "" });
   }
 
   render() {
     return (
       <div className="form-group input-group">
         <input
-          ref="input" type="text" className="form-control" onKeyPress={(e) => e.charCode === 13 ? this.onAdd() : null}
-          placeholder="Add a domain (example.com or sub.example.com)"
+          type="text" className="form-control" placeholder="Add a domain (example.com or sub.example.com)"
+          value={this.state.domain} onKeyPress={(e) => e.charCode === 13 ? this.onAdd() : null}
+          onChange={this.handleChange}
         />
         <span className="input-group-btn">
           <button onClick={this.onAdd} className="btn btn-secondary" type="button">
