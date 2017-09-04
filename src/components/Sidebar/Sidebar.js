12d9fb8da9016ad9925835421ce1f289c6f3eaca
Mcat12 <newtoncat12@yahoo.com>
2017-09-04 17:55:47 -0400

    Update sidebar with wildlist
    Also update the file with "paraphrased" code from the upstream CoreUI
    project:
    https://github.com/mrholek/CoreUI-React/blob/master/React_Starter/src/components/Sidebar/Sidebar.js
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,59 +1,94 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
+import { Nav, NavItem } from 'reactstrap';
 import logo from '../../img/logo.svg';
 
-//const handleClick = (e) => {
-//  e.preventDefault();
-//  e.target.parentElement.classList.toggle('open');
-//};
+const nav = {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'fa fa-dashboard'
+    },
+    {
+      name: 'Query Log',
+      url: '/query-log',
+      icon: 'fa fa-database'
+    },
+    {
+      name: 'Whitelist',
+      url: '/whitelist',
+      icon: 'fa fa-check-square-o'
+    },
+    {
+      name: 'Blacklist',
+      icon: 'fa fa-ban',
+      children: [
+        {
+          name: 'Exact',
+          url: '/blacklist',
+          icon: 'fa fa-ban'
+        },
+        {
+          name: 'Wildcard',
+          url: '/wildlist',
+          icon: 'fa fa-ban'
+        }
+      ]
+    }
+  ]
+};
 
-//const activeRoute = (routeName) => {
-//  return this.props.location.pathname.indexOf(routeName) > -1 ? 'nav-item nav-dropdown open' : 'nav-item nav-dropdown';
-//}
+const handleClick = (e) => {
+  e.preventDefault();
+  e.target.parentElement.classList.toggle('open');
+};
 
-//const secondLevelActive = (routeName) => {
-//  return this.props.location.pathname.indexOf(routeName) > -1 ? "nav nav-second-level collapse in" : "nav nav-second-level collapse";
-//}
+const activeRoute = (routeName, props) =>
+  props.location.pathname.indexOf(routeName) > -1 ? 'nav-item nav-dropdown open' : 'nav-item nav-dropdown';
 
-export default () => (
-  <div className="sidebar">
-    <nav className="sidebar-nav">
-      <ul className="nav">
-        <li className="nav-title">
-          <img src={logo} className="img-responsive pull-left" style={{height: "67px"}} alt=""/>
-          <p className="pull-left"
-             style={{
-               paddingLeft: "15px", textTransform: "initial", fontSize: "14px", marginBottom: "initial",
-               lineHeight: "14px", color: "white"
-             }}>
-            Pi-hole
-          </p>
-          <br/>
-          <span style={{textTransform: "initial", paddingLeft: "15px"}}>
-            <i className="fa fa-circle text-success"/> Online
-          </span>
-        </li>
-        <li className="nav-item">
-          <NavLink to={'/dashboard'} className="nav-link" activeClassName="active">
-            <i className="fa fa-dashboard"/> Dashboard
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to={'/query-log'} className="nav-link" activeClassName="active">
-            <i className="fa fa-database"/> Query Log
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to={'/whitelist'} className="nav-link" activeClassName="active">
-            <i className="fa fa-check-square-o"/> Whitelist
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to={'/blacklist'} className="nav-link" activeClassName="active">
-            <i className="fa fa-ban"/> Blacklist
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </div>
+const navItem = (item, key) => (
+  <NavItem key={key}>
+    <NavLink to={item.url} className="nav-link" activeClassName="active">
+      <i className={item.icon}/>{item.name}
+    </NavLink>
+  </NavItem>
 );
+
+const navDropdown = (item, key, props) => (
+  <li key={key} className={activeRoute(item.url, props)}>
+    <a className="nav-link nav-dropdown-toggle" style={{ "cursor": "pointer" }} onClick={handleClick}><i className={item.icon}/>{item.name}</a>
+    <ul className="nav-dropdown-items">
+      {navList(item.children)}
+    </ul>
+  </li>
+);
+
+const navList = (items, props) =>
+  items.map((item, index) => item.children ? navDropdown(item, index, props) : navItem(item, index));
+
+export default props => {
+  return (
+    <div className="sidebar">
+      <nav className="sidebar-nav">
+        <Nav>
+          <li className="nav-title">
+            <img src={logo} className="img-responsive pull-left" style={{height: "67px"}} alt=""/>
+            <p className="pull-left"
+               style={{
+                 paddingLeft: "15px", textTransform: "initial", fontSize: "14px", marginBottom: "initial",
+                 lineHeight: "14px", color: "white"
+               }}>
+              Pi-hole
+            </p>
+            <br/>
+            <span style={{textTransform: "initial", paddingLeft: "15px"}}>
+              <i className="fa fa-circle text-success"/> Online
+            </span>
+          </li>
+          {navList(nav.items, props)}
+        </Nav>
+      </nav>
+    </div>
+  )
+}
