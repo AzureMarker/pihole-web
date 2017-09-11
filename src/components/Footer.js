8d7021b8201c0cb02e48af2d3a5730200469ebe9
Mcat12 <newtoncat12@yahoo.com>
2017-09-11 19:35:21 -0400

    Change bottom left donate icon to PayPal
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,14 +14,16 @@ import React from 'react';
 export default () => (
   <footer className="app-footer">
     <div className="pull-right">
-      <b className="hidden-xs-down">Core Version: </b><b className="hidden-sm-up">C: </b> <span id="piholeVersion">vDev</span>
-      <b className="hidden-xs-down"> Admin Version: </b><b className="hidden-sm-up"> A: </b> <span id="webVersion">vDev</span>
+      <b className="hidden-xs-down">Core Version: </b>
+      <b className="hidden-sm-up">C: </b> <span id="piholeVersion">vDev</span>
+      <b className="hidden-xs-down"> Admin Version: </b>
+      <b className="hidden-sm-up"> A: </b> <span id="webVersion">vDev</span>
     </div>
     <div>
-      <i className="fa fa-github"/> <strong>
-      <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=3J2L3Z4DHW9UY">
-        Donate
-      </a></strong> if you found this useful
+      <i className="fa fa-paypal"/>
+      <strong>
+        <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=3J2L3Z4DHW9UY"> Donate</a>
+      </strong> if you found this useful
     </div>
   </footer>
 );
