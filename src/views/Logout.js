c0f78c26b9429d8ee647a2fcfb325a33a0c84653
Mcat12 <newtoncat12@yahoo.com>
2018-04-30 15:23:16 -0400

    Remove finished logout todo
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/views/Logout.js
+++ src/views/Logout.js
@@ -13,7 +13,6 @@ import { api } from "../utils";
 
 export default class Logout extends Component {
   componentWillMount() {
-    // TODO: remove auth cookie
     api.loggedIn = false;
     api.logout();
     this.props.history.push('/');
