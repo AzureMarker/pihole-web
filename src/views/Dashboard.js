fb6cd051272e506de6066a7e311ebc676e10ef55
Mcat12 <newtoncat12@yahoo.com>
2018-06-04 20:32:27 -0400

    Remove forwarded over time and hide query types over time graphs
    Forwarded over time was removed in FTL and will be removed from the API,
    and query types was removed from the PHP web interface and will have its
    fate decided later.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -11,8 +11,6 @@
 import React, { Fragment } from 'react';
 import SummaryStats from '../components/SummaryStats';
 import QueriesGraph from '../components/QueriesGraph';
-import QueryTypesOverTime from '../components/QueryTypesOverTime';
-import ForwardDestOverTime from '../components/ForwardDestOverTime';
 import TopDomains from '../components/TopDomains';
 import TopBlocked from '../components/TopBlocked';
 import TopClients from '../components/TopClients';
@@ -26,8 +24,6 @@ export default () => (
       api.loggedIn ?
         <Fragment>
           <div className="row">
-            <QueryTypesOverTime/>
-            <ForwardDestOverTime/>
           </div>
           <div className="row">
             <TopDomains/>
