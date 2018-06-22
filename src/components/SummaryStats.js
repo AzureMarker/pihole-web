1b728b590ca96709d80db037af73d75ff79de031
Rob Gill <rrobgill@protonmail.com>
2018-06-22 14:09:29 +1000

    Add Icons to cards
    Adds background images to cards.
    -css to position them within the card
    -Fontawesome icons for the four cards in SummaryStats
    
    Signed-off-by: Rob Gill <rrobgill@protonmail.com>

--- src/components/SummaryStats.js
+++ src/components/SummaryStats.js
@@ -65,6 +65,10 @@ class SummaryStats extends Component {
         <div className="col-lg-3 col-xs-12">
           <div className="card card-inverse card-success stat-height-lock">
             <div className="card-block">
+              <div className="card-icon">
+                <i className="fa fa-globe fa-2x">
+                </i>
+              </div>
               <h3>{this.state.totalQueries}</h3>
               <p style={{marginBottom: "0px"}}>
                 {t("Total Queries ({{count}} clients)", { count: this.state.uniqueClients })}
@@ -75,6 +79,10 @@ class SummaryStats extends Component {
         <div className="col-lg-3 col-xs-12">
           <div className="card card-inverse card-primary stat-height-lock">
             <div className="card-block">
+              <div className="card-icon">
+                <i className="fa fa-hand-stop-o fa-2x">
+                </i>
+              </div>
               <h3>{this.state.blockedQueries}</h3>
               <p style={{marginBottom: "0px"}}>
                 {t("Queries Blocked")}
@@ -85,6 +93,10 @@ class SummaryStats extends Component {
         <div className="col-lg-3 col-xs-12">
           <div className="card card-inverse card-warning stat-height-lock">
             <div className="card-block">
+              <div className="card-icon" style={{"z-index": "100"}}>
+                <i className="fa fa-pie-chart fa-2x">
+                </i>
+              </div>
               <h3>{this.state.percentBlocked}</h3>
               <p style={{marginBottom: "0px"}}>
                 {t("Percent Blocked")}
@@ -95,6 +107,10 @@ class SummaryStats extends Component {
         <div className="col-lg-3 col-xs-12">
           <div className="card card-inverse card-danger stat-height-lock">
             <div className="card-block">
+              <div className="card-icon">
+                 <i className="fa fa-list-alt fa-2x">
+                 </i>
+              </div>
               <h3>{this.state.gravityDomains}</h3>
               <p style={{marginBottom: "0px"}}>
                 {t("Domains On Blocklist")}
