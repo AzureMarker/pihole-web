016d9cf8f2107d3caf2ab6c12a103d97871d0c0f
Mcat12 <newtoncat12@yahoo.com>
2018-06-28 22:03:29 -0400

    Add some simple tests for Alert and BoxedLayoutButton
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

---
+++ src/components/Alert.test.js
@@ -0,0 +1,31 @@
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2017 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  Web Interface
+*  Alert component test
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license. */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import Alert from './Alert';
+
+it('renders an alert correctly', () => {
+  const message = "test message";
+  const onClick = jest.fn();
+  const wrapper = shallow(<Alert type="info" message={message} onClick={onClick}/>);
+
+  expect(wrapper).toHaveClassName("alert-info");
+  expect(wrapper).toIncludeText(message);
+});
+
+it('calls onClick when clicked', () => {
+  const message = "test message";
+  const onClick = jest.fn();
+  const wrapper = shallow(<Alert type="info" message={message} onClick={onClick}/>);
+
+  wrapper.find("button").simulate("click");
+  expect(onClick).toBeCalled();
+});
