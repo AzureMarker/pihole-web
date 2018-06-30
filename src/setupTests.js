eb3690c68d3f32406eb6342506bbf896e2d751d7
Mcat12 <newtoncat12@yahoo.com>
2018-06-30 16:46:05 -0400

    Mock out react-i18next to easily test translated components
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/setupTests.js
+++ src/setupTests.js
@@ -12,4 +12,14 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
+// Setup enzyme
 configure({ adapter: new Adapter() });
+
+// Mock out react-i18next
+jest.mock('react-i18next', () => ({
+  // This mock makes sure any components using the translate HoC receive the t function as a prop
+  translate: () => component => {
+    component.defaultProps = { ...component.defaultProps, t: key => key };
+    return component;
+  },
+}));
