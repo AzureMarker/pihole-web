21b240a1fcc8761d081b21752f39fd658f737484
Mcat12 <newtoncat12@yahoo.com>
2018-06-30 17:37:21 -0400

    Reset api.loggedIn before each test
    Temporary fix to reset logged in state for each test.
    
    The permanent fix would be to not use a global variable, and instead
    give this information to components through props. Redux would be good
    for this, if we want to add it to the project.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/setupTests.js
+++ src/setupTests.js
@@ -11,6 +11,7 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
+import { api } from "./utils";
 
 // Setup enzyme
 configure({ adapter: new Adapter() });
@@ -23,3 +24,10 @@ jest.mock('react-i18next', () => ({
     return component;
   },
 }));
+
+// Temporary fix to reset logged in state for each test.
+// The permanent fix would be to not use a global variable, and instead give this information to components through
+// props. Redux would be good for this, if we want to add it to the project.
+beforeEach(() => {
+  api.loggedIn = false;
+});
