e9641b03bd7a3c7ec2706f8e75eb1262aa662e39
Mcat12 <newtoncat12@yahoo.com>
2018-08-18 14:02:43 -0400

    Improve version cards with general version and detailed version info
    The versions page is more responsive as well.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/components/settings/VersionInfo.js
+++ src/components/settings/VersionInfo.js
@@ -87,19 +87,19 @@ class VersionInfo extends Component {
 
     return (
       <div className="row">
-        <div className="col-lg-3 col-xs-12">
+        <div className="col-xl-3 col-md-6 col-xs-12">
           <VersionCard name={t("Core")} icon="fa fa-dot-circle-o fa-2x"
                        branch={this.state.core.branch} hash={this.state.core.hash} tag={this.state.core.tag} t={t}/>
         </div>
-        <div className="col-lg-3 col-xs-12">
+        <div className="col-xl-3 col-md-6 col-xs-12">
           <VersionCard name={t("FTL")} icon="fa fa-industry fa-2x"
                        branch={this.state.ftl.branch} hash={this.state.ftl.hash} tag={this.state.ftl.tag} t={t}/>
         </div>
-        <div className="col-lg-3 col-xs-12">
+        <div className="col-xl-3 col-md-6 col-xs-12">
           <VersionCard name={t("API")} icon="fa fa-bullseye fa-2x"
                        branch={this.state.api.branch} hash={this.state.api.hash} tag={this.state.api.tag} t={t}/>
         </div>
-        <div className="col-lg-3 col-xs-12">
+        <div className="col-xl-3 col-md-6 col-xs-12">
           <VersionCard name={t("Web")} icon="fa fa-list-alt fa-2x"
                        branch={this.state.web.branch} hash={this.state.web.hash} tag={this.state.web.tag} t={t}/>
         </div>
