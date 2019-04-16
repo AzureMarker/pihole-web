3f3bed566e42b6e55ad8b8969a91012b97920cc1
Mcat12 <newtoncat12@yahoo.com>
2019-04-15 19:52:49 -0700

    Bring list folder coverage to 100% by adding/improving ListPage tests
    Also used `jest.spyOn` where possible to avoid directly manipulating
    the component's methods.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/components/list/__tests__/ListPage.test.tsx
+++ src/components/list/__tests__/ListPage.test.tsx
@@ -15,6 +15,9 @@ import ListPage, {
   ListPageProps,
   ListPageState
 } from "../ListPage";
+import Alert from "../../common/Alert";
+import DomainInput from "../DomainInput";
+import DomainList from "../DomainList";
 
 const ignoreAPI = global.ignoreAPI;
 const tick = global.tick;
@@ -58,7 +61,7 @@ it("shows the placeholder", () => {
     />
   );
 
-  expect(wrapper.find("DomainInput")).toHaveProp("placeholder", placeholder);
+  expect(wrapper.find(DomainInput)).toHaveProp("placeholder", placeholder);
 });
 
 it("shows the note", () => {
@@ -93,7 +96,97 @@ it("starts with no alerts shown", () => {
     />
   );
 
-  expect(wrapper.find("Alert")).toHaveLength(0);
+  expect(wrapper.find(Alert)).toHaveLength(0);
+});
+
+it("hides the alert if closed", () => {
+  const wrapper: ListPageWrapper = shallow(
+    <ListPage
+      title=""
+      placeholder=""
+      note=""
+      onAdd={ignoreAPI}
+      onRefresh={ignoreAPI}
+      onRemove={ignoreAPI}
+      isValid={jest.fn()}
+      validationErrorMsg=""
+    />
+  );
+
+  // Show an error message
+  wrapper.instance().onAlreadyAdded("domain");
+
+  // Now the alert is shown
+  const alert = wrapper.find(Alert);
+  expect(alert).toHaveLength(1);
+
+  // Hide the alert
+  alert.props().onClick();
+
+  expect(wrapper.find(Alert)).toHaveLength(0);
+});
+
+it("cancels requests when un-mounting", async () => {
+  const wrapper: ListPageWrapper = shallow(
+    <ListPage
+      title=""
+      placeholder=""
+      note=""
+      onAdd={ignoreAPI}
+      onRefresh={() => Promise.resolve(["domain"])}
+      onRemove={ignoreAPI}
+      isValid={jest.fn()}
+      validationErrorMsg=""
+    />
+  );
+
+  // Load the domains into state
+  await tick();
+
+  // Initiate requests to refresh, add, and remove domains
+  wrapper.instance().onRefresh();
+  wrapper.instance().onEnter("domain2");
+  wrapper.instance().onRemove("domain");
+
+  // Spy on the handlers
+  // Casting instance to any to access private fields
+  const instance = wrapper.instance() as any;
+  const cancelRefreshSpy = jest.spyOn(instance.refreshHandler, "cancel");
+  const cancelAddSpy = jest.spyOn(instance.addHandler, "cancel");
+  const cancelRemoveSpy = jest.spyOn(instance.removeHandler, "cancel");
+
+  // Unmount, which should cancel the requests
+  wrapper.unmount();
+
+  expect(cancelRefreshSpy).toHaveBeenCalled();
+  expect(cancelAddSpy).toHaveBeenCalled();
+  expect(cancelRemoveSpy).toHaveBeenCalled();
+});
+
+it("shows a validation message as an error", () => {
+  const validationError = "test message";
+  const wrapper = shallow(
+    <ListPage
+      title=""
+      placeholder=""
+      note=""
+      onAdd={ignoreAPI}
+      onRefresh={ignoreAPI}
+      onRemove={ignoreAPI}
+      isValid={jest.fn()}
+      validationErrorMsg={validationError}
+    />
+  );
+
+  wrapper
+    .find(DomainInput)
+    .props()
+    .onValidationError();
+
+  const alert = wrapper.find(Alert);
+  expect(alert).toHaveLength(1);
+  expect(alert.props().message).toEqual(validationError);
+  expect(alert.props().type).toEqual("danger");
 });
 
 it("loads domains after mounting", async () => {
@@ -112,14 +205,12 @@ it("loads domains after mounting", async () => {
   );
 
   await tick();
-  wrapper.update();
 
-  expect(wrapper.find("DomainList")).toHaveProp("domains", domains);
+  expect(wrapper.find(DomainList)).toHaveProp("domains", domains);
 });
 
 it("checks if the domain was already added", async () => {
   const domains = ["domain1", "domain2.com", "domain3.net"];
-  const onAlreadyAdded = jest.fn();
   const wrapper: ListPageWrapper = shallow(
     <ListPage
       title=""
@@ -132,13 +223,11 @@ it("checks if the domain was already added", async () => {
       validationErrorMsg=""
     />
   );
+  const onAlreadyAdded = jest.spyOn(wrapper.instance(), "onAlreadyAdded");
 
-  // Setup with domains (wait for promise to resolve) and mock function
+  // Setup with domains (wait for promise to resolve)
   await tick();
-  wrapper.instance().onAlreadyAdded = onAlreadyAdded;
-  wrapper.update();
 
-  // Test onEnter
   wrapper.instance().onEnter(domains[0]);
 
   expect(onAlreadyAdded).toHaveBeenCalledWith(domains[0]);
@@ -146,13 +235,13 @@ it("checks if the domain was already added", async () => {
 
 it("calls the add prop when adding a domain", () => {
   const domain = "domain";
-  const add = jest.fn(ignoreAPI);
+  const onAdd = jest.fn(ignoreAPI);
   const wrapper: ListPageWrapper = shallow(
     <ListPage
       title=""
       placeholder=""
       note=""
-      onAdd={add}
+      onAdd={onAdd}
       onRefresh={ignoreAPI}
       onRemove={ignoreAPI}
       isValid={jest.fn()}
@@ -162,12 +251,11 @@ it("calls the add prop when adding a domain", () => {
 
   wrapper.instance().onEnter(domain);
 
-  expect(add).toHaveBeenCalledWith(domain);
+  expect(onAdd).toHaveBeenCalledWith(domain);
 });
 
 it("calls onAdding when adding a domain", () => {
   const domain = "domain";
-  const onAdding = jest.fn();
   const wrapper: ListPageWrapper = shallow(
     <ListPage
       title=""
@@ -180,9 +268,8 @@ it("calls onAdding when adding a domain", () => {
       validationErrorMsg=""
     />
   );
+  const onAdding = jest.spyOn(wrapper.instance(), "onAdding");
 
-  wrapper.instance().onAdding = onAdding;
-  wrapper.update();
   wrapper.instance().onEnter(domain);
 
   expect(onAdding).toHaveBeenCalledWith(domain);
@@ -190,7 +277,6 @@ it("calls onAdding when adding a domain", () => {
 
 it("calls onAdded after API request succeeds", async () => {
   const domain = "domain";
-  const onAdded = jest.fn();
   const wrapper: ListPageWrapper = shallow(
     <ListPage
       title=""
@@ -203,9 +289,8 @@ it("calls onAdded after API request succeeds", async () => {
       validationErrorMsg=""
     />
   );
+  const onAdded = jest.spyOn(wrapper.instance(), "onAdded");
 
-  wrapper.instance().onAdded = onAdded;
-  wrapper.update();
   wrapper.instance().onEnter(domain);
   await tick();
 
@@ -214,7 +299,6 @@ it("calls onAdded after API request succeeds", async () => {
 
 it("calls onAddFailed after API request fails", async () => {
   const domain = "domain";
-  const onAddFailed = jest.fn();
   const wrapper: ListPageWrapper = shallow(
     <ListPage
       title=""
@@ -227,9 +311,8 @@ it("calls onAddFailed after API request fails", async () => {
       validationErrorMsg=""
     />
   );
+  const onAddFailed = jest.spyOn(wrapper.instance(), "onAddFailed");
 
-  wrapper.instance().onAddFailed = onAddFailed;
-  wrapper.update();
   wrapper.instance().onEnter(domain);
   await tick();
 
@@ -252,7 +335,6 @@ it("adds the domain in onAdded", async () => {
   );
 
   wrapper.instance().onEnter(domain);
-  wrapper.update();
   await tick();
 
   expect(wrapper.state().domains).toEqual([domain]);
@@ -274,15 +356,14 @@ it("resets the domains when adding failed", async () => {
   );
 
   wrapper.instance().onEnter(domain);
-  wrapper.update();
   await tick();
 
   expect(wrapper.state().domains).toEqual([]);
 });
 
-it("removes the domain when onRemoved is called", async () => {
-  const domain = "domain";
-  const domains = [domain];
+it("does not remove the domain if it is not present", async () => {
+  const domain = "domain1";
+  const domains = ["domain2"];
   const wrapper: ListPageWrapper = shallow(
     <ListPage
       title=""
@@ -296,13 +377,36 @@ it("removes the domain when onRemoved is called", async () => {
     />
   );
 
-  wrapper.instance().onRemoved(domain);
-  wrapper.update();
+  await tick();
+  wrapper.instance().onRemove(domain);
 
-  expect(wrapper.state().domains).toEqual([]);
+  expect(wrapper.state().domains).toEqual(domains);
 });
 
-it("resets the domains when removal failed", () => {
+it("removes the domain from state when onRemove is called", async () => {
+  const domain = "domain";
+  const domain2 = "domain2";
+  const domains = [domain, domain2];
+  const wrapper: ListPageWrapper = shallow(
+    <ListPage
+      title=""
+      placeholder=""
+      note=""
+      onAdd={ignoreAPI}
+      onRefresh={() => Promise.resolve(domains)}
+      onRemove={() => Promise.resolve()}
+      isValid={jest.fn()}
+      validationErrorMsg=""
+    />
+  );
+
+  await tick();
+  wrapper.instance().onRemove(domain);
+
+  expect(wrapper.state().domains).toEqual([domain2]);
+});
+
+it("resets the domains when removal failed", async () => {
   const domain = "domain";
   const domains = [domain];
   const wrapper: ListPageWrapper = shallow(
@@ -312,13 +416,15 @@ it("resets the domains when removal failed", () => {
       note=""
       onAdd={ignoreAPI}
       onRefresh={() => Promise.resolve(domains)}
-      onRemove={ignoreAPI}
+      onRemove={() => Promise.reject()}
       isValid={jest.fn()}
       validationErrorMsg=""
     />
   );
-  wrapper.instance().onRemoveFailed(domain, domains);
-  wrapper.update();
+
+  await tick();
+  wrapper.instance().onRemove(domain);
+  await tick();
 
   expect(wrapper.state().domains).toEqual(domains);
 });
