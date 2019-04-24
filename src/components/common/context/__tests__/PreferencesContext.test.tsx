9898d2e1a161cb1acbd27ac67492ef205ed82414
Mcat12 <newtoncat12@yahoo.com>
2019-04-23 20:37:57 -0700

    Improve context test coverage to 100%
    To properly test React hooks, React needed to be updated to at least
    16.8.5, along with `react-test-renderer`.
    
    To properly test the WithAPIData render props, enzyme needed to be
    updated to at least 3.8.0.
    
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- src/components/common/context/__tests__/PreferencesContext.test.tsx
+++ src/components/common/context/__tests__/PreferencesContext.test.tsx
@@ -3,16 +3,21 @@
  * Network-wide ad blocking via your own hardware.
  *
  * Web Interface
- * React context object tests
+ * React preferences context tests
  *
  * This file is copyright under the latest version of the EUPL.
  * Please see LICENSE file for your rights under this license. */
 
+import React from "react";
 import {
   defaultPreferences,
   loadInitialPreferences,
-  WEB_PREFERENCES_STORAGE_KEY
+  WEB_PREFERENCES_STORAGE_KEY,
+  PreferencesProvider,
+  PreferencesContextType
 } from "../PreferencesContext";
+import { shallow } from "enzyme";
+import { WithAPIData } from "../../WithAPIData";
 
 it("loads the default preferences if none are cached", () => {
   const preferences = loadInitialPreferences();
@@ -34,4 +39,81 @@ it("loads the cached preferences when available", () => {
   const actualPreferences = loadInitialPreferences();
 
   expect(actualPreferences).toEqual(expectedPreferences);
+});
+
+it("loads the default preferences if it fails to parse the cached one", () => {
+  localStorage.setItem(WEB_PREFERENCES_STORAGE_KEY, "not valid JSON");
+
+  const preferences = loadInitialPreferences();
+
+  expect(preferences).toEqual(defaultPreferences);
+});
+
+it("provides the initial settings while loading", () => {
+  const expectedPreferences = loadInitialPreferences();
+
+  const wrapper = shallow(<PreferencesProvider>{null}</PreferencesProvider>)
+    .find(WithAPIData)
+    .renderProp("renderInitial")();
+
+  expect(wrapper.props().value.settings).toEqual(expectedPreferences);
+});
+
+it("caches the settings after loading and provides the new settings", () => {
+  const expectedPreferences: ApiPreferences = {
+    language: "testLang",
+    layout: "boxed"
+  };
+  const refresh = jest.fn();
+  const expectedSettings: PreferencesContextType = {
+    settings: expectedPreferences,
+    refresh
+  };
+
+  const wrapper = shallow(<PreferencesProvider>{null}</PreferencesProvider>)
+    .find(WithAPIData)
+    .renderProp("renderOk")(expectedPreferences, refresh);
+
+  expect(wrapper.props().value).toEqual(expectedSettings);
+  expect(localStorage.getItem(WEB_PREFERENCES_STORAGE_KEY)).toEqual(
+    JSON.stringify(expectedPreferences)
+  );
+});
+
+it("loads the default preferences if there's an error and its not cached", () => {
+  const expectedPreferences = loadInitialPreferences();
+  const refresh = jest.fn();
+  const expectedSettings: PreferencesContextType = {
+    settings: expectedPreferences,
+    refresh
+  };
+
+  const wrapper = shallow(<PreferencesProvider>{null}</PreferencesProvider>)
+    .find(WithAPIData)
+    .renderProp("renderErr")(null, refresh);
+
+  expect(wrapper.props().value).toEqual(expectedSettings);
+});
+
+it("loads the cached preferences if there's an error and its cached", () => {
+  const expectedPreferences: ApiPreferences = {
+    language: "testLang",
+    layout: "boxed"
+  };
+  const refresh = jest.fn();
+  const expectedSettings: PreferencesContextType = {
+    settings: expectedPreferences,
+    refresh
+  };
+
+  localStorage.setItem(
+    WEB_PREFERENCES_STORAGE_KEY,
+    JSON.stringify(expectedPreferences)
+  );
+
+  const wrapper = shallow(<PreferencesProvider>{null}</PreferencesProvider>)
+    .find(WithAPIData)
+    .renderProp("renderErr")(null, refresh);
+
+  expect(wrapper.props().value).toEqual(expectedSettings);
 });
