e972d56e7c3c39979074d4a1f1a3cdd45e1b2cf1
Adam Warner <me@adamwarner.co.uk>
2019-11-26 20:09:35 +0000

    let -> const
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- src/util/dateUtils.tsx
+++ src/util/dateUtils.tsx
@@ -9,12 +9,12 @@
  * Please see LICENSE file for your rights under this license. */
 
 export function getTimeFromTimestamp(input: number) {
-  let date = new Date(input * 1000);
-  let hours = date.getHours();
-  let minutes = "0" + date.getMinutes();
-  let seconds = "0" + date.getSeconds();
+  const date = new Date(input * 1000);
+  const hours = date.getHours();
+  const minutes = "0" + date.getMinutes();
+  const seconds = "0" + date.getSeconds();
   // Will display time in 10:30:23 format
-  let formattedTime =
+  const formattedTime =
     hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
 
   return formattedTime;
