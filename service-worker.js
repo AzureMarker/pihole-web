17ff92798a1d0bb4bbb4c72e8052ea863de0d3c5
Mcat12 <Mcat12@users.noreply.github.com>
2020-09-06 20:32:26 +0000

    deploy: a400c4c152e4f82f4d557363caa46a94dc5412d0

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a9d8e1727828b5c36aa35c48d3050fe.js"
+  "/pihole-web/precache-manifest.e87f0d5bae269f224acf434fbfcd73cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pihole-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
